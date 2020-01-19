aa84c07988ab192aebe2d66ef799212a5377baad
Michael Braha <mbraha@localhost.localdomain>
2020-01-19 11:23:41 -0800

    hover popup feature

--- app/main/routes.py
+++ app/main/routes.py
@@ -222,3 +222,11 @@ def search():
                            posts=posts,
                            next_url=next_url,
                            prev_url=prev_url)
+
+
+# For small popup of user on hover. Very simliar to profile route.
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
